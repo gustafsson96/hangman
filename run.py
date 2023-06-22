6c14019390dbf3b809399f49be572b7d1c8fbd04
gustafsson96 <>
2023-06-22 00:29:06 +0000

    Add list of words and hangman art

--- run.py
+++ run.py
@@ -1,1 +1,61 @@
 import random
+
+list_of_words = [
+    'horse',
+    'dog',
+    'cat',
+    'hamster',
+    'apple',
+    'orange',
+    'pear',
+    'strawberry',
+    'banana',
+    'pineapple',
+    'carrot',
+    'cucumber',
+    'food',
+    'student',
+    'drink',
+    'developer',
+    'fish',
+    'snake',
+    'bubble',
+    'wedding',
+    'dress',
+    'music',
+    'book',
+    'phone',
+    'flower',
+    'rose',
+    'bag',
+    'table',
+    'chair',
+    'mother',
+    'father',
+    'sister',
+    'brother',
+    'friend',
+    'scarf',
+    'house',
+    'hand',
+    'foot',
+    'face',
+    'heart',
+    'elephant',
+    'tornado',
+    'thunder',
+    'rain',
+    'summer',
+    'winter',
+    'fall',
+    'spring']
+
+HANGMAN_ART = '''
+*----
+|   |
+|   O
+|  /|\ 
+|  / \ 
+|
+'-------
+'''

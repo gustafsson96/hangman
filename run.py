a64f97a0fad4577927f2b8f5a6b49cad49764f4d
gustafsson96 <>
2023-06-22 00:33:50 +0000

    Add main game function to ask for and validate users guess

--- run.py
+++ run.py
@@ -90,3 +90,55 @@ print(f'\nOkay, {username}, let\'s start guessing!\n')
 def get_word():
     word = random.choice(list_of_words)  # pick random word from list
     return word.upper()
+
+
+ def play(word):
+    word_completion = '_' * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print('Lets play hangman!')
+    print(tries)
+    print(word_completion)
+    print('\n')
+    while not guessed and tries > 0:
+        guess = input('Please guess a letter or word: ').upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print('you alreay guessed this letter!')
+            elif guess not in word:
+                print(guess, 'is not in word')
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print('good job', guess, 'is the word')
+                guessed_letters.append(guess)
+                print(guessed_letters)
+                word_as_list = list(word_completion)
+                indicies = [
+                    i for i, letter in enumerate(word) if letter == guess]
+                for index in indicies:
+                    word_as_list[index] = guess
+                word_completion = ''.join(word_as_list)
+                if '_' not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print('you already guessed this word', guess)
+            elif guess != word:
+                print(guess, 'is not the word')
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print('not a valid guess')
+        print(tries)
+        print(word_completion)
+        print('\n')
+    if guessed:
+        print('you guessed the word, you win!')
+    else:
+        print('sorry, you ran out of tries')   
